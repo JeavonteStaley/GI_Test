b9139e7e97286b465acf0c3855ddf1f16509a788
JeavonteStaley <staleyjs@mail.uc.edu>
2019-02-09 10:01:19 -0500

    Commit 2

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Loop #: " + i);
+        }
+        System.out.println("---------------------------");
+        for (int x = 1; x <= 10; ++x){
+            System.out.println("Loop #: " + x);
+        }
+    }
+}
