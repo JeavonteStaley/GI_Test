80a2c25e26e009564da62c11ea858e9d1e9ad444
JeavonteStaley <staleyjs@mail.uc.edu>
2019-02-09 10:09:49 -0500

    Update

--- src/Loops.java
+++ src/Loops.java
@@ -7,5 +7,11 @@ public class Loops {
         for (int x = 1; x <= 10; ++x){
             System.out.println("Loop #: " + x);
         }
+        System.out.println("-----------------------");
+        for(int h = 1; h <= 100; h++){
+            if(h % 3 == 0 && h % 5 == 0) {
+                System.out.println("Loop #: " + h);
+            }
+        }
     }
 }
